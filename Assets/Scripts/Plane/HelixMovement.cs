e85e9928adfcb37ada6d21c3b8bdc5329221ba95
Matías Pacheco <matipacheco@gmail.com>
2021-01-06 21:26:57 -0300

    first commit. Adding plane prefab and adding helix movement

---
+++ Assets/Scripts/Plane/HelixMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class HelixMovement : MonoBehaviour {
+    [SerializeField] private float speed = 200f;
+
+    void Update() {
+        transform.Rotate(Vector3.left * speed * Time.deltaTime);
+    }
+}
