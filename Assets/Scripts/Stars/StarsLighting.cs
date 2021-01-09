9a28db6fe8ad6880a7cbc6c976f1846f0f0da293
Matías Pacheco <matipacheco@gmail.com>
2021-01-09 18:52:16 -0300

    finally, adding two different light sources for the sun and the moon

---
+++ Assets/Scripts/Stars/StarsLighting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class StarsLighting : MonoBehaviour {
+    private bool isDay;
+    [SerializeField] private Transform universeCenter;
+    [SerializeField] private Light sunLight;
+    [SerializeField] private Light moonLight;
+
+    private void Update() {
+        SetIsDay();
+        Debug.Log(isDay);
+        
+
+        // sunLight.gameObject.SetActive(isDay);
+        // moonLight.gameObject.SetActive(!isDay);
+    }
+
+    private float Distance() {
+        return transform.position.y - universeCenter.position.y;
+    }
+
+    public void SetIsDay() {
+        isDay = (Distance() >= 0);
+    }
+
+    public bool IsDay() {
+        return isDay;
+    }
+
+    private void TriggerDawn() {}
+    private void TriggerSunset() {}
+}
